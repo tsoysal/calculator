403c57d1f2d5b8c085dbd18fc5226dab8968b81d
berksysl <berksysl.g@gmail.com>
2023-08-12 13:37:53 +0300

    Create calculator logic

--- script.js
+++ script.js
@@ -18,14 +18,111 @@ darkBtn.addEventListener('click', () => {
 })
 
 // Display buttons
+let firstNum;
+let secondNum;
+let operator;
 
 const display = document.querySelector('#display');
 const miniDisplay = document.querySelector('.miniDisplay');
 
 const buttons = document.querySelectorAll('button');
 
-function handleBtnClick(btn) {
-    display.innerText += btn.innerText;
+function calculate() {
+    switch(operator) {
+        case '+': 
+            return firstNum + secondNum;
+        case '-': 
+            return firstNum - secondNum;
+        case '*':
+            return firstNum * secondNum;
+        case '/':
+            return firstNum / secondNum;
+        case '%':
+            return firstNum % secondNum;
+    }
 }
 
-buttons.forEach((btn) => btn.addEventListener('click', () => handleBtnClick(btn)))
+function handleOperators(btnText) {
+    if(display.innerText == ''){
+        return;
+    }
+    switch(btnText){
+        case 'AC': 
+            display.innerText = '';
+            miniDisplay.innerText = '';
+            break;
+        case '+-':
+            display.innerText *= -1;
+            break; 
+        case '<-':
+            display.innerText = display.innerText.slice(0, display.innerText.length -1);
+            break;
+        case '.':
+            if(!display.innerText.includes('.')){
+                display.innerText += '.';
+            }
+            break;
+        case '%':
+            firstNum = parseInt(display.innerText);
+            operator = '%';
+            display.innerText += '%';
+            miniDisplay.innerText = display.innerText;
+            display.innerText = '';
+            break;
+        case '+':
+            // Get the first number
+            firstNum = parseInt(display.innerText);
+            // Set the operator
+            operator = '+';
+            // Print '+' to the screen
+            display.innerText += '+';
+            // Move to mini display
+            miniDisplay.innerText = display.innerText;
+            // Clear the main display
+            display.innerText = '';
+            break;
+        case '-':
+            firstNum = parseInt(display.innerText);
+            operator = '-';
+            display.innerText += '-';
+            miniDisplay.innerText = display.innerText;
+            display.innerText = '';
+            break;
+        case 'x':
+            firstNum = parseInt(display.innerText);
+            operator = '*';
+            display.innerText += 'x';
+            miniDisplay.innerText = display.innerText;
+            display.innerText = '';
+            break;
+        case '/':
+            firstNum = parseInt(display.innerText);
+            operator = '/';
+            display.innerText += '/';
+            miniDisplay.innerText = display.innerText;
+            display.innerText = '';
+            break;
+        case '=':
+            if(operator == undefined){
+                return;
+            }
+            secondNum = parseInt(display.innerText);
+            display.innerText += '=';
+            miniDisplay.innerText += display.innerText;
+            display.innerText = calculate();
+            operator = undefined;
+            firstNum = undefined;
+            secondNum = undefined;
+            break;
+    }
+}
+
+function handleBtnClick(btnText) {
+    if(isNaN(btnText)){
+        handleOperators(btnText);
+        return;
+    }
+    display.innerText += btnText;
+}
+
+buttons.forEach((btn) => btn.addEventListener('click', () => handleBtnClick(btn.innerText)))
